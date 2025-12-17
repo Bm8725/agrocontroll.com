ba3c6099942a99dc70354ece50c609da7c0aba37
Bm8725 <211398250+Bm8725@users.noreply.github.com>
2025-12-17 17:05:42 +0200

    Update : menu, responsive

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,13 +21,28 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Navbar />
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <head>
+        {/* Crisp Chat Script */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              window.$crisp = [];
+              window.CRISP_WEBSITE_ID = "7541a482-977d-4867-9a96-944d7c8b4871";
+              (function(){
+                var d = document;
+                var s = d.createElement("script");
+                s.src = "https://client.crisp.chat/l.js";
+                s.async = 1;
+                d.getElementsByTagName("head")[0].appendChild(s);
+              })();
+            `,
+          }}
+        />
+      </head>
+      <body>
         {children}
+        <Footer />
       </body>
-      <Footer />
     </html>
   );
 }
